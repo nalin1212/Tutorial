4ff9e8e9763a52ca1c8400e0e2ebd3097f9a4c7f
nalin1212 <145797322+nalin1212@users.noreply.github.com>
2026-03-11 17:35:51 +0530

    frist commit

---
+++ 13.3.class.py
@@ -0,0 +1,14 @@
+class empolyee:
+    name="harry" 
+    language = "py" # this is class attribute
+    salary =120000
+
+harry= empolyee()
+harry.language = "java script  " #this is instance 0r object  attribute 
+print( harry.name,harry.language) 
+
+#  rohan = empolyee()
+#  rohan.name ="robinson"
+#  print (rohan.name ,rohan.salary,rohan. language )
+
+# Here name is object attribute and salary and language are class attribute as they directly belong  to the class
